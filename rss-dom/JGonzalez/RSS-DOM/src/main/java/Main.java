d5d95333e1f1d8670ee384f3f4317e124f467ac5
Javier González Muñoz <javiergmun@gmail.com>
2021-10-14 10:36:21 +0200

    Primer commit JGM

---
+++ rss-dom/JGonzalez/RSS-DOM/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Archivo arch = new Archivo();
+        arch.procesarDoc();
+        System.out.println("\n\t*** YA NO HAY MÁS QUE LEER, VUELVE EN UN RATO ***");
+    }
+}
+/**
+ * REFERENCIAS QUE HE TENIDO EN CUENTA
+ * https://developer.mozilla.org/es/docs/Web/API/Document/querySelectorAll
+ * http://jmoral.es/blog/xml-dom
+ * https://docs.python.org/es/3.8/library/xml.dom.html
+ */
