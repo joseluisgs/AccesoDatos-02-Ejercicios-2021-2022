33d67ef0c3c7634fafa1079da83d54e56745a6b8
Adrian <adrian.loz@hotmail.com>
2021-10-17 15:04:28 +0200

    trabajo rss-dom finalizado tal y como se pide

---
+++ rss-dom/ALozano/src/main/java/org/alozano/Noticia.java
@@ -0,0 +1,31 @@
+package org.alozano;
+
+import lombok.Data;
+
+import java.io.Serializable;
+@Data
+public class Noticia implements Serializable {
+
+    private String titulo;
+    private String categoria;
+    private String descripcion;
+    private String fecha;
+    private String enlace;
+    private String author;
+    private static int num=1;
+    private int count;
+
+    public String toString() {
+        return "Mi Noticia: " +count+ "\n"+
+                " Titulo: " + this.titulo + "\n" +
+                " Categoria: " + this.categoria + "\n" +
+                " Descripción: " + this.descripcion + "\n" +
+                " Fecha: " + this.fecha + "\n" +
+                " Enlace: " + this.enlace + "\n" +
+                " Autor: " + this.author + "\n";
+    }
+    public Noticia(){
+        count = num;
+        num++;
+    }
+}
