f722e22ba9e866dab1faddd9cd443460761495ce
Federico Toledo <federicotoledobaeza@gmail.com>
2021-10-18 22:58:51 +0200

    gitflow-feature-stash: FSToledo/rss-sax

---
+++ rss-sax/FSToledo/src/main/java/dam/sax/SAXController.java
@@ -0,0 +1,38 @@
+package dam.sax;
+
+import dam.sax.model.News;
+import lombok.NonNull;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.util.List;
+
+public class SAXController {
+    private static SAXController controller;
+    private final String uri;
+    private List<News> newsList;
+
+    private SAXController(String uri){
+        this.uri=uri;
+    }
+
+    public static SAXController getInstance(@NonNull String uri){
+        if(controller == null) controller = new SAXController(uri);
+        return controller;
+    }
+
+    public void loadData() throws ParserConfigurationException, IOException, SAXException {
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        SAXParser saxParser = saxParserFactory.newSAXParser();
+        NewsHandler newsHandler = new NewsHandler();
+        saxParser.parse(this.uri,newsHandler);
+        this.newsList = newsHandler.getNews();
+    }
+    public List<News> getNews(){
+        return this.newsList;
+    }
+
+}
