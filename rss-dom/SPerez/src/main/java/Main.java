953425eb64219dc5106636f47be1405a9e0c6224
Sergio <sps169@outlook.es>
2021-10-21 23:46:07 +0200

    XMLFile parse to Object RSSItem completed

--- rss-dom/SPerez/src/main/java/Main.java
+++ rss-dom/SPerez/src/main/java/Main.java
@@ -1,12 +1,12 @@
 import DOMController.DOMController;
 
 public class Main {
-	public static final String XML_URI = "";
+	public static final String XML_URI = "http://ep00.epimg.net/rss/tags/ultimas_noticias.xml";
 	public static void main (String[]args) {
 		DOMController xmlParser = DOMController.getInstance(XML_URI);
 		try{
 			xmlParser.loadData();
-			xmlParser.printXML(System.out);
+			xmlParser.getRSSItems().forEach(System.out::println);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
